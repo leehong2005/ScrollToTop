130b2d17236cfefd2549f3bf665925b6946d88f6
李洪 <lh104439@alibaba-inc.com>
2016-11-03 11:36:29 +0800

    Add the view classes

---
+++ library/src/main/java/com/megabox/android/scroll/ui/ScrollToTopListView.java
@@ -0,0 +1,24 @@
+package com.megabox.android.scroll.ui;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ListView;
+
+/**
+ * @author lihong
+ * @since 2016/11/03
+ */
+public class ScrollToTopListView extends ListView {
+
+    public ScrollToTopListView(Context context) {
+        super(context);
+    }
+
+    public ScrollToTopListView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ScrollToTopListView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
---
+++ library/src/main/java/com/megabox/android/scroll/ui/ScrollToTopScrollView.java
@@ -0,0 +1,8 @@
+package com.megabox.android.scroll.ui;
+
+/**
+ * @author lihong
+ * @since 2016/11/03
+ */
+public class ScrollToTopScrollView {
+}
